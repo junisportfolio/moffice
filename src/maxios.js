5af5cbd60582157f195351ab3f1963061f630570
inhyukpark <megatic@mocobee.com>
2017-03-21 17:50:51 +0900

    기타 수정
    html lang="ko"
    환경변수에 의거 maxios.help() 브라우져에 노출되지 않도록

--- src/maxios.js
+++ src/maxios.js
@@ -7,28 +7,28 @@ class maxios {
       withCredentials: true
     });
 
-    if(process.env.NODE_ENV === 'development') {
-      this.help();
-    }
+    this.help();
   }
 
   help() {
-    window.maxios = this;
-    
-    const message = "Maxios\nCustom plugin, for Handling ajax calls, for Moffice application. It based on axios library.\n" +
-      "\n" +
-      "Getting started by typing [import maxios from '../maxios';] into your source.\n" +
-      "GET 요청: maxios.get(url[, paramObject[, successCallback]]);\n" +
-      "paramObject와 successCallback은 생략 가능합니다. paramObject는 {key0: value0, key1: value1} => '?key0=value0&key1=value1'로 전환됩니다.\n\n" +
-      "POST 요청: maxios.post(url, dataObject, successCallback);\n" +
-      "dataObject는 JSON 타입으로 전달해주세요.\n\n" +
-      "PUT 요청: maxios.put(url, dataObject, successCallback);\n" +
-      "dataObject는 JSON 타입으로 전달해주세요.\n\n" +
-      "DELETE 요청: maxios.delete(url, successCallback);\n\n" +
-      "모든 요청 메소드에 공통적으로 successCallback 인자를 통해 maxios.console 객체를 전달하면 기본 출력 메소드를 호출할 수 있습니다.\n" +
-      "maxios 사용법을 알고 싶으시면 개발중인 소스에서 언제든지 maxios.help()를 호출해주세요.\n" +
-      "%c이 메세지가 보인 화면에서는 웹브라우져를 통해 maxios 객체에 접근할 수 있습니다. 이 메세지가 라이브 환경에서 보여선 안됩니다.";
-    console.log(message, "background: red; color: white");
+    if(process.env.NODE_ENV === 'development') {
+      window.maxios = this;
+
+      const message = "Maxios\nCustom plugin, for Handling ajax calls, for Moffice application. It based on axios library.\n" +
+        "\n" +
+        "Getting started by typing [import maxios from '../maxios';] into your source.\n" +
+        "GET 요청: maxios.get(url[, paramObject[, successCallback]]);\n" +
+        "paramObject와 successCallback은 생략 가능합니다. paramObject는 {key0: value0, key1: value1} => '?key0=value0&key1=value1'로 전환됩니다.\n\n" +
+        "POST 요청: maxios.post(url, dataObject, successCallback);\n" +
+        "dataObject는 JSON 타입으로 전달해주세요.\n\n" +
+        "PUT 요청: maxios.put(url, dataObject, successCallback);\n" +
+        "dataObject는 JSON 타입으로 전달해주세요.\n\n" +
+        "DELETE 요청: maxios.delete(url, successCallback);\n\n" +
+        "모든 요청 메소드에 공통적으로 successCallback 인자를 통해 maxios.console 객체를 전달하면 기본 출력 메소드를 호출할 수 있습니다.\n" +
+        "maxios 사용법을 알고 싶으시면 개발중인 소스에서 언제든지 maxios.help()를 호출해주세요.\n" +
+        "%c이 메세지가 보인 화면에서는 웹브라우져를 통해 maxios 객체에 접근할 수 있습니다. 이 메세지가 라이브 환경에서 보여선 안됩니다.";
+      console.log(message, "background: red; color: white");
+    }
   }
 
   console(data) {
